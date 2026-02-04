323db7660334d24254328b23be53a9c73408d74f
Josue0675 <josuesan0672@gmail.com>
2026-02-03 18:42:01 -0600

    Clase 1

---
+++ tienda_josuegarciasanchez/src/main/java/tienda_josuegarciasanchez/demo/TiendaJosuegarciasanchezApplication.java
@@ -0,0 +1,14 @@
+package tienda_josuegarciasanchez.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TiendaJosuegarciasanchezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJosuegarciasanchezApplication.class, args);
+	}
+
+}
---
+++ tienda_josuegarciasanchez/src/test/java/tienda_josuegarciasanchez/demo/TiendaJosuegarciasanchezApplicationTests.java
@@ -0,0 +1,13 @@
+package tienda_josuegarciasanchez.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaJosuegarciasanchezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
